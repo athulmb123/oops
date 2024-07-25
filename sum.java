23342689b1c422003c6a22c074eb2eac48af83fe
athulmb123 <vksinu0@gmail.com>
2024-07-25 15:13:28 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class sum
+{
+	public static void main(String args[])
+	{
+		Scanner s1=new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter the two numbers:");
+		a=s1.nextInt();
+		b=s1.nextInt();
+		int c=a+b;
+		System.out.println("The Sum of "+a+" and "+b+" is:"+c);
+		s1.close();
+	}
+}
+		
+	
